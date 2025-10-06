b1596962957321f7d07cc57dc9c044fd2f68092d
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-05 23:48:21 -0700

    imported imgs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import imgGlobe from "../images/globe.png";
+
 export default function Header() {
     return (
         <header>
-            <img src="../images/globe.png" alt="globe icon" />
+            <img src={imgGlobe} alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
     )
